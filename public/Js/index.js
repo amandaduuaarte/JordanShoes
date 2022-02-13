a75f230db1667e1f71507237e486f87785b3e73e
Amanda <amandaduuaarte@gmail.com>
2022-02-13 15:11:35 -0300

    Adicionando imagens ao projeto

---
+++ public/Js/index.js
@@ -0,0 +1,8 @@
+const arrayImages = [1,2,3,4,5,6,7,8];
+const getDiv = document.querySelector(".imagesContainer");
+const divImage = document.createElement('div');
+
+
+while (arrayImages.length > 0) {
+
+}
